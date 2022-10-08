739ddd59f4cea5823c25fdbfb3666bc9e9eb25c7
Hanbi58 <hanxiaonan2012@gmail.com>
2022-10-08 16:31:45 -0700

    Nodelist also has index

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
-const contents = document.querySelectorAll(".content");
-const lis = document.querySelectorAll("li");
-const arr = Array.from(lis);
+// const contents = document.querySelectorAll(".content");
+// const lis = document.querySelectorAll("li");
+// const arr = Array.from(lis);
 
-lis.forEach((li) => {
-  li.addEventListener("click", (e) => {
-    removeLiClass();
-    li.classList.add("active");
-    removeImgClass();
-    const contentShowIdx = arr.indexOf(li);
-    contents[contentShowIdx].classList.add("show");
+// lis.forEach((li) => {
+//   li.addEventListener("click", (e) => {
+//     removeLiClass();
+//     li.classList.add("active");
+//     removeImgClass();
+//     const contentShowIdx = arr.indexOf(li);
+//     contents[contentShowIdx].classList.add("show");
+//   });
+// });
+
+// function removeLiClass() {
+//   lis.forEach((li) => {
+//     li.classList.remove("active");
+//   });
+// }
+// function removeImgClass() {
+//   contents.forEach((content) => {
+//     content.classList.remove("show");
+//   });
+// }
+
+const contents = document.querySelectorAll(".content");
+const listItems = document.querySelectorAll("nav ul li");
+
+listItems.forEach((item, idx) => {
+  item.addEventListener("click", () => {
+    hideAllContents();
+    hideAllItems();
+    item.classList.add("active");
+    contents[idx].classList.add("show");
   });
 });
 
-function removeLiClass() {
-  lis.forEach((li) => {
-    li.classList.remove("active");
-  });
+function hideAllContents() {
+  contents.forEach((content) => content.classList.remove("show"));
 }
-function removeImgClass() {
-  contents.forEach((content) => {
-    content.classList.remove("show");
-  });
+function hideAllItems() {
+  listItems.forEach((item) => item.classList.remove("active"));
 }
